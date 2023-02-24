51a6c849dfaa24cdae002edcecd777871e7e601f
ingaSiu <ingasiud@gmail.com>
2023-02-24 20:34:06 +0200

    Full name fnc

---
+++ src/utils/string.ts
@@ -0,0 +1,6 @@
+export const getFullName = (firstName: any, lastName: any): any => {
+  return `${firstName} ${lastName}`;
+};
+
+// parasyti funkcija, kuri prideda zvaigzdutes pagal password ilgi
+//function that coverts string to * e.g. rokas => *****
