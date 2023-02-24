1353fcab33ac167fca2e27f7cfacd501182627f5
ingaSiu <ingasiud@gmail.com>
2023-02-24 20:35:24 +0200

    ts lesson

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,6 +1,8 @@
 // reactNode yra ne tik component, bet ir kitu tipu elementai
 
 // pridedam type path, kad apsisaugoti, jei norim naujo path, reikia cia prideti
+// pasirasom path, kurie jau egzistuoja
+
 export type Route = {
   path: '/' | '/users';
   Component: () => JSX.Element;
