f7ab8395a5d2ce3046db01ff09d06480df64945a
ingaSiu <ingasiud@gmail.com>
2023-02-27 12:10:51 +0200

    jobs page

--- src/pages/Jobs/Jobs.tsx
+++ src/pages/Jobs/Jobs.tsx
@@ -1,4 +1,11 @@
+import { useJobs } from '../../hooks/jobs';
+import { Job } from '../../types/jobApi';
+
 const Jobs = () => {
+  const { data } = useJobs();
+  const jobs = data || [];
+
+  console.log(jobs);
   return <div>Jobs</div>;
 };
 
