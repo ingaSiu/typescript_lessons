91d8a41fd5dd25fab6c6a6c6f49efb348b1155a2
ingaSiu <ingasiud@gmail.com>
2023-02-27 12:09:40 +0200

    hidePsw func added

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,6 +1,10 @@
-export const getFullName = (firstName: any, lastName: any): any => {
+export const getFullName = (firstName: string, lastName: string): string => {
   return `${firstName} ${lastName}`;
 };
 
 // parasyti funkcija, kuri prideda zvaigzdutes pagal password ilgi
 //function that coverts string to * e.g. rokas => *****
+export const hidePassword = (password: string): string => {
+  const hiddenPsw = '*'.repeat(password.length);
+  return hiddenPsw;
+};
